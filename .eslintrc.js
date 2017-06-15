a9b4412d856f9e3a73c17b6a6fbf07871a484e2b
mirreal <zgkm181142521@gmail.com>
2017-06-15 19:34:09 +0800

    update: add rollup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6,
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
       experimentalObjectRestSpread: true
