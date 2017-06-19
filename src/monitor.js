b533062b27a1182fb2bf28dad06d760dedfeba8d
mirreal <zgkm181142521@gmail.com>
2017-06-19 17:28:47 +0800

    update: 更改 API

--- src/monitor.js
+++ src/monitor.js
@@ -7,6 +7,13 @@ import {
 var monitor = {}
 monitor.tryJS = tryJS
 
+setting({ handleTryCatchError: handleTryCatchError })
+
+monitor.init = function(opts) {
+  __config(opts)
+  __init()
+}
+
 // 忽略错误监听
 window.ignoreError = false
 // UA
@@ -20,20 +27,6 @@ var config = {
   delay: 2000, // 错误处理间隔时间
   maxError: 16, // 异常报错数量限制
   sampling: 1 // 采样率
-}
-
-setting({
-  handleTryCatchError: handleTryCatchError
-})
-
-monitor.config = function(opts) {
-  merge(opts, config)
-
-  // TODO
-  // 去除默认 report 函数的执行
-  report = debounce(config.report, config.delay, function() {
-    errorList = []
-  })
 }
 
 // 定义的错误类型码
@@ -54,38 +47,48 @@ var LOAD_ERROR_TYPE = {
   VIDEO: ERROR_VIDEO
 }
 
-// 监听 JavaScript 报错异常(JavaScript runtime error)
-window.onerror = function() {
-  if (window.ignoreError) {
-    window.ignoreError = false
-    return
-  }
+function __config(opts) {
+  merge(opts, config)
 
-  handleError(formatRuntimerError.apply(null, arguments))
+  report = debounce(config.report, config.delay, function() {
+    errorList = []
+  })
 }
 
-// 监听资源加载错误(JavaScript Scource failed to load)
-window.addEventListener('error', function(event) {
-  // 过滤 target 为 window 的异常，避免与上面的 onerror 重复
-  var errorTarget = event.target
-  if (errorTarget !== window && errorTarget.nodeName && LOAD_ERROR_TYPE[errorTarget.nodeName.toUpperCase()]) {
-    handleError(formatLoadError(errorTarget))
-  }
-}, true)
-
-// 针对 vue 报错重写 console.error
-// TODO
-console.error = (function(origin) {
-  return function(info) {
-    var errorLog = {
-      type: ERROR_CONSOLE,
-      desc: info
+function __init() {
+  // 监听 JavaScript 报错异常(JavaScript runtime error)
+  window.onerror = function() {
+    if (window.ignoreError) {
+      window.ignoreError = false
+      return
     }
 
-    handleError(errorLog)
-    origin.call(console, info)
+    handleError(formatRuntimerError.apply(null, arguments))
   }
-})(console.error)
+
+  // 监听资源加载错误(JavaScript Scource failed to load)
+  window.addEventListener('error', function(event) {
+    // 过滤 target 为 window 的异常，避免与上面的 onerror 重复
+    var errorTarget = event.target
+    if (errorTarget !== window && errorTarget.nodeName && LOAD_ERROR_TYPE[errorTarget.nodeName.toUpperCase()]) {
+      handleError(formatLoadError(errorTarget))
+    }
+  }, true)
+
+  // 针对 vue 报错重写 console.error
+  // TODO
+  console.error = (function(origin) {
+    return function(info) {
+      var errorLog = {
+        type: ERROR_CONSOLE,
+        desc: info
+      }
+
+      handleError(errorLog)
+      origin.call(console, info)
+    }
+  })(console.error)
+}
 
 // 处理 try..catch 错误
 function handleTryCatchError(error) {
