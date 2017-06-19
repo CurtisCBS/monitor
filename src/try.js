89510948d78a84b45840fbab6fb16a46841d231d
mirreal <zgkm181142521@gmail.com>
2017-06-19 11:27:21 +0800

    update: ensure only wrap the function once

--- src/try.js
+++ src/try.js
@@ -21,15 +21,20 @@ export function setting(opts) {
  * @return {Function} 包装后的函数
  */
 function tryify(func) {
-  return function() {
-    try {
-      return func.apply(this, arguments)
-    } catch (error) {
-      config.handleCatchError(error)
+  // 确保只包装一次
+  if (!func._wrapped) {
+    func._wrapped = function() {
+      try {
+        return func.apply(this, arguments)
+      } catch (error) {
+        config.handleCatchError(error)
 
-      throw error
+        throw error
+      }
     }
   }
+
+  return func._wrapped
 }
 
 /**
